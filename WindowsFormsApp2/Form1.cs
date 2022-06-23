415dd724e8734e57db9140ebe59df97cfb0b2bc2
Андрей Нарчу <narvik1221@mail.ru>
2022-06-23 13:27:25 +0600

    Добавлены базовые функции калькулятора

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -56,6 +56,7 @@ namespace WindowsFormsApp2
         {
             double dn1, dn2, res;
             res = 0;
+
             dn1 = Convert.ToDouble(N1);
             dn2 = Convert.ToDouble(textBox1.Text);
             if (D == "+")
