e6367c4489d8b586a28d6c06060cbcd90b3ead5f
Андрей Нарчу <narvik1221@mail.ru>
2022-06-28 13:58:36 +0600

    Выполнено тестирование фабрик

---
+++ WindowsFormsApp2.Tests/UnitTest2.cs
@@ -0,0 +1,30 @@
+﻿
+using NUnit.Framework;
+using System;
+// чтобы работало в управлениях пакетами NuGet (пкм по решению проекта) нужно установить NunitTestAdapter
+namespace WindowsFormsApp2.Tests
+{
+    [TestFixture]
+    public class UnitTest2
+    {
+        [Test]
+        [TestCase("substract", typeof(SubtractionCalculator))]
+        [TestCase("Multiplic", typeof(MultiplicationCalculator))]
+        [TestCase("Division", typeof(DivisionCalculator))]
+        [TestCase("Addition", typeof(AdditionalCalculator))]
+        [TestCase("Pow", typeof(PowCalculator))]
+        [TestCase("AntiPow", typeof(AntiPowCalculator))]
+        [TestCase("Sred", typeof(SredCalculator))]
+        [TestCase("", typeof(ZeroCalculator))]
+        
+
+  
+        public void CreateCalculatorTest(string name, Type type)
+        {
+            var calculator =TwoArgFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(  type, calculator);
+        }
+
+    }
+}
